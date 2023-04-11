connorkuehl/factoid

commit c1dfb3058909f33b8d81ad7a0d31fe23f80a49c6

Merge read & write servers into a single server

main.go
@@ -59,20 +59,13 @@ func main() {
 	public := httprouter.New()
 	public.HandlerFunc(http.MethodGet, "/v1/facts", service.FactsHandler)
 	public.HandlerFunc(http.MethodGet, "/v1/fact/:id", service.FactHandler)
-
-	private := httprouter.New()
-	private.HandlerFunc(http.MethodPost, "/v1/facts", service.FactsHandler)
-	private.HandlerFunc(http.MethodDelete, "/v1/fact/:id", service.FactHandler)
-	private.Handler(http.MethodGet, "/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
+	public.HandlerFunc(http.MethodPost, "/v1/facts", service.FactsHandler)
+	public.HandlerFunc(http.MethodDelete, "/v1/fact/:id", service.FactHandler)
+	public.Handler(http.MethodGet, "/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
 
 	publicServer := http.Server{
 		Addr:    config.readAddr,
 		Handler: withMetrics(metrics, public),
-	}
-
-	privateServer := http.Server{
-		Addr:    config.writeAddr,
-		Handler: withMetrics(metrics, private),
 	}
 
 	serve := func(s *http.Server, name string) {
@@ -84,7 +77,6 @@ func main() {
 	}
 
 	go serve(&publicServer, "http_public")
-	go serve(&privateServer, "http_private")
 
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, os.Interrupt)
 	defer cancel()
@@ -98,7 +90,6 @@ func main() {
 	ctx, cancel = context.WithTimeout(ctx, 3*time.Second)
 	defer cancel()
 
-	privateServer.Shutdown(ctx)
 	publicServer.Shutdown(ctx)
 
 	log.Info().Err(ctx.Err()).Msg("shut down")

contrib/import-csv/import-csv.go
@@ -17,7 +17,7 @@ func main() {
 		file string
 	)
 
-	flag.StringVar(&addr, "addr", "http://localhost:8081/v1", "REST API addr")
+	flag.StringVar(&addr, "addr", "http://localhost:8080/v1", "REST API addr")
 	flag.StringVar(&file, "csv", "", "path to CSV")
 	flag.Parse()
 

commit 741578ea4313bbc88a0992f2c8b61e0d8ab05d02

Consolidate command line flags
Accidentally left this out from the referenced commit.

Fixes: c1dfb3058909f33b8d81ad7a0d31fe23f80a49c6

main.go
@@ -21,13 +21,11 @@ import (
 
 func main() {
 	var config struct {
-		readAddr   string
-		writeAddr  string
+		addr       string
 		sqlitePath string
 	}
 
-	flag.StringVar(&config.readAddr, "api-public", ":8080", "public API address")
-	flag.StringVar(&config.writeAddr, "api-private", ":8081", "private API address")
+	flag.StringVar(&config.addr, "addr", ":8080", "address to listen on")
 	flag.StringVar(&config.sqlitePath, "db-sqlite", ":memory:", "path to SQLite DB")
 	flag.Parse()
 
@@ -55,7 +53,7 @@ func main() {
 	mux.Handler(http.MethodGet, "/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
 
 	server := http.Server{
-		Addr:    config.readAddr,
+		Addr:    config.addr,
 		Handler: withMetrics(metrics, mux),
 	}
 